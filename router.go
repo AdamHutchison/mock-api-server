5c78d66c2358c913d58d5534b75505372b014023
Adam Hutchison <adam.hutchison@gophr.it>
2021-10-26 20:58:30 +0100

    Adds common error routes

--- router.go
+++ router.go
@@ -11,8 +11,23 @@ func RegisterRoutes(mux *mux.Router) {
 		&h.DummyHandler{FilePath: "./schemas/test.json"},
 	)
 
+	// Error Routes
+	mux.Handle(
+		"/api/error/401",
+		&h.DummyErrorHandler{Status: 401},
+	)
+
 	mux.Handle(
 		"/api/error/404",
 		&h.DummyErrorHandler{Status: 404},
+	)
+	mux.Handle(
+		"/api/error/422",
+		&h.DummyErrorHandler{Status: 422},
+	)
+
+	mux.Handle(
+		"/api/error/500",
+		&h.DummyErrorHandler{Status: 500},
 	)
 }
